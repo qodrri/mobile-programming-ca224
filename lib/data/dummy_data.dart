f09260024571934714f8eada75bb10bed4f3e67f
Julius <flowinthedream@gmail.com>
2024-11-23 20:54:07 +0800

    update

--- lib/data/dummy_data.dart
+++ lib/data/dummy_data.dart
@@ -6,13 +6,15 @@ final List<Hewan> dummyHewan = [
     namaSpesies: 'Panthera tigris',
     namaIndonesia: 'Harimau',
     deskripsi: 'Harimau adalah kucing besar dengan corak belang.',
-    gambar: 'assets/images/tiger.jpg',
+    gambar:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Tigress_at_Jim_Corbett_National_Park.jpg/800px-Tigress_at_Jim_Corbett_National_Park.jpg',
   ),
   Hewan(
     id: '2',
     namaSpesies: 'Elephas maximus',
     namaIndonesia: 'Gajah',
     deskripsi: 'Gajah adalah hewan darat terbesar.',
-    gambar: 'assets/images/elephant.jpg',
+    gambar:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/African_Bush_Elephant.jpg/800px-African_Bush_Elephant.jpg',
   ),
 ];
