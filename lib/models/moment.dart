2452abb94bfb082541c79aed9a28cde76e90ce54
I Wayan Jepriana <jepriana@sahabatlautlestari.com>
2024-12-14 16:48:52 +0800

    feat: data serializer and deserializer

--- lib/models/moment.dart
+++ lib/models/moment.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class Moment {
   String id;
   String creator;
@@ -20,4 +22,58 @@ class Moment {
     this.commentsCount = 0,
     this.bookmarksCount = 0,
   });
+
+  Moment copyWith({
+    String? id,
+    String? creator,
+    String? location,
+    DateTime? momentDate,
+    String? caption,
+    String? imageUrl,
+    int? likesCount,
+    int? commentsCount,
+    int? bookmarksCount,
+  }) {
+    return Moment(
+      id: id ?? this.id,
+      creator: creator ?? this.creator,
+      location: location ?? this.location,
+      momentDate: momentDate ?? this.momentDate,
+      caption: caption ?? this.caption,
+      imageUrl: imageUrl ?? this.imageUrl,
+      likesCount: likesCount ?? this.likesCount,
+      commentsCount: commentsCount ?? this.commentsCount,
+      bookmarksCount: bookmarksCount ?? this.bookmarksCount,
+    );
+  }
+
+  factory Moment.fromMap(Map<String, dynamic> json) {
+    return Moment(
+      id: json['id'],
+      creator: json['creator'],
+      location: json['location'],
+      momentDate: DateTime.parse(json['momentDate']),
+      caption: json['caption'],
+      imageUrl: json['imageUrl'],
+      likesCount: json['likesCount'],
+      commentsCount: json['commentsCount'],
+      bookmarksCount: json['bookmarksCount'],
+    );
+  }
+
+  Map<String, dynamic> toMap() => {
+        'id': id,
+        'creator': creator,
+        'location': location,
+        'momentDate': momentDate.toIso8601String(),
+        'caption': caption,
+        'imageUrl': imageUrl,
+        'likesCount': likesCount,
+        'commentsCount': commentsCount,
+        'bookmarksCount': bookmarksCount,
+      };
+
+  factory Moment.fromJson(String json) => Moment.fromMap(jsonDecode(json));
+
+  String toJson() => jsonEncode(toMap());
 }
